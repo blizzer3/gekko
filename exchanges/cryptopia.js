3dc4612804968eb1e3118b9e59c9f81ef611c2c2
Andrew Throener <athroener@whitneys-mbp.lan>
2018-01-19 22:43:56 -0600

    Update

--- exchanges/cryptopia.js
+++ exchanges/cryptopia.js
@@ -55,56 +55,7 @@ var recoverableErrors = new RegExp(
   /(SOCKETTIMEDOUT|TIMEDOUT|CONNRESET|CONNREFUSED|NOTFOUND|Rate limit exceeded|Response code 5)/
 );
 
-Trader.prototype.processError = function(funcName, error) {
-  if (!error) return undefined;
-
-  if (!error.message.match(recoverableErrors)) {
-    log.error(
-      `[cryptopia] (${funcName}) returned an irrecoverable error: ${
-        error.message
-      }`
-    );
-    return new Errors.AbortError('[cryptopia] ' + error.message);
-  }
-
-  log.debug(
-    `[cryptopia] (${funcName}) returned an error, retrying: ${error.message}`
-  );
-  return new Errors.RetryError('[cryptopia] ' + error.message);
-};
-
-Trader.prototype.handleResponse = function(funcName, callback) {
-  return (error, response, body) => {
-    if (body && !_.isEmpty(body.message)) error = new Error(body.message);
-    else if (
-      response &&
-      response.statusCode < 200 &&
-      response.statusCode >= 300
-    )
-      error = new Error(`Response code ${response.statusCode}`);
-
-    return callback(this.processError(funcName, error), body);
-  };
-};
-
 Trader.prototype.getPortfolio = function(callback) {
-  /*
-  var result = function(err, data) {
-    if (err) return callback(err);
-
-    var portfolio = data.map(function(account) {
-      return {
-        name: account.currency.toUpperCase(),
-        amount: parseFloat(account.available),
-      };
-    });
-    callback(undefined, portfolio);
-  };
-
-  let handler = cb =>
-    this.gdax.getAccounts(this.handleResponse('getPortfolio', cb));
-  util.retryCustom(retryForever, _.bind(handler, this), _.bind(result, this));
-  */
   this.private.getBalance()
     .then(balances => this.mapBalances)
     .then(balances => callback(null, balances))
@@ -121,128 +72,54 @@ Trader.prototype.mapBalances = function (balances) {
 }
 
 Trader.prototype.getTicker = function(callback) {
-  console.log("getTicker", this);
-  /*
-  var result = function(err, data) {
-    if (err) return callback(err);
-    callback(undefined, { bid: +data.bid, ask: +data.ask });
-  };
-
-  let handler = cb =>
-    this.gdax_public.getProductTicker(this.handleResponse('getTicker', cb));
-  util.retryCustom(retryForever, _.bind(handler, this), _.bind(result, this));
-  */
-
   this.public.fetchTicker(this.pair).then(ticker => {
-    console.log("Ticker", ticker);
     callback(null, { bid: ticker.bid, ask: ticker.ask })
   })
   .catch(err => callback(err, null));
-
 };
 
 Trader.prototype.getFee = function(callback) {
-  /*
-  //https://www.gdax.com/fees
-  const fee = this.asset == 'BTC' ? 0.0025 : 0.003;
-
-  //There is no maker fee, not sure if we need taker fee here
-  //If post only is enabled, gdax only does maker trades which are free
-  callback(undefined, this.post_only ? 0 : fee);
-  */
   return  callback(null, 0.0002);
 };
 
 Trader.prototype.buy = function(amount, price, callback) {
-  /*
-  var buyParams = {
-    price: this.getMaxDecimalsNumber(price, this.currency == 'BTC' ? 5 : 2),
-    size: this.getMaxDecimalsNumber(amount),
-    product_id: this.pair,
-    post_only: this.post_only,
-  };
-
-  var result = (err, data) => {
-    if (err) return callback(err);
-    callback(undefined, data.id);
-  };
-
-  let handler = cb => this.gdax.buy(buyParams, this.handleResponse('buy', cb));
-  util.retryCustom(retryCritical, _.bind(handler, this), _.bind(result, this));
-  */
+  this.private.createLimitBuyOrder(this.transformPair(), amount, price)
+    .then(order => callback(null, order))
+    .catch(err => {
+      util.retryCustom(retryCritical, this.buy.apply(this, arguments), callback);
+    });
 };
 
 Trader.prototype.sell = function(amount, price, callback) {
-  /*
-  var sellParams = {
-    price: this.getMaxDecimalsNumber(price, this.currency == 'BTC' ? 5 : 2),
-    size: this.getMaxDecimalsNumber(amount),
-    product_id: this.pair,
-    post_only: this.post_only,
-  };
-
-  var result = function(err, data) {
-    if (err) return callback(err);
-    callback(undefined, data.id);
-  };
-
-  let handler = cb =>
-    this.gdax.sell(sellParams, this.handleResponse('buy', cb));
-  util.retryCustom(retryCritical, _.bind(handler, this), _.bind(result, this));
-  */
+  this.private.createLimitSellOrder(this.transformPair(), amount, price)
+    .then(order => callback(null, order))
+    .catch(err => {
+      util.retryCustom(retryCritical, this.buy.apply(this, arguments), callback);
+    });
 };
 
 Trader.prototype.checkOrder = function(order, callback) {
-  /*
-  var result = function(err, data) {
-    if (err) return callback(err);
-
-    var status = data.status;
-    if (status == 'done') {
-      return callback(undefined, true);
-    } else if (status == 'rejected') {
-      return callback(undefined, false);
-    } else if (status == 'pending') {
-      return callback(undefined, false);
-    }
-    callback(undefined, false);
-  };
-
-  let handler = cb =>
-    this.gdax.getOrder(order, this.handleResponse('checkOrder', cb));
-  util.retryCustom(retryCritical, _.bind(handler, this), _.bind(result, this));
-  */
+  this.private.fetchOrder(order.id)
+    .then(order => callback(null, order.status === "closed" && order.remaining === 0 ? true : false))
+    .catch(err => {
+      util.retryCustom(retryCritical, this.buy.apply(this, arguments), callback);
+    });
 };
 
 Trader.prototype.getOrder = function(order, callback) {
-  /*
-  var result = function(err, data) {
-    if (err) return callback(err);
-
-    var price = parseFloat(data.price);
-    var amount = parseFloat(data.filled_size);
-    var date = moment(data.done_at);
-
-    callback(undefined, { price, amount, date });
-  };
-
-  let handler = cb =>
-    this.gdax.getOrder(order, this.handleResponse('getOrder', cb));
-  util.retryCustom(retryForever, _.bind(handler, this), _.bind(result, this));
-  */
+  this.private.fetchOrder(order.id)
+    .then(order => callback(null, { price: order.price, amount: order.amount, date: moment.unix(order.timestamp) }))
+    .catch(err => {
+      util.retryCustom(retryCritical, this.buy.apply(this, arguments), callback);
+    });
 };
 
 Trader.prototype.cancelOrder = function(order, callback) {
-  /*
-  var result = function(err, data) {
-    // todo, verify result..
-    callback();
-  };
-
-  let handler = cb =>
-    this.gdax.cancelOrder(order, this.handleResponse('cancelOrder', cb));
-  util.retryCustom(retryForever, _.bind(handler, this), _.bind(result, this));
-  */
+  this.private.cancelOrder(order.id)
+    .then(order => callback(null))
+    .catch(err => {
+      util.retryCustom(retryCritical, this.buy.apply(this, arguments), callback);
+    });
 };
 
 Trader.prototype.transformPair = function () {
